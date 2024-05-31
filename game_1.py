6ea5d1e4df8af3e2eb698f9196e8c1bc1a3a3320
P1owerpump <vladblinskiy@gmail.com>
2024-05-31 06:25:14 +0500

    First commit

---
+++ game_1.py
@@ -0,0 +1,22 @@
+"""Game: Guess the number"""
+
+import numpy as np
+
+number = np.random.randint(1, 101) # Случайное число от 1 до 100
+
+# Количество попыток
+count = 0
+
+while True:
+    count += 1
+    predict_number = int(input('Угадай число от 1 до 100: '))
+    
+    if predict_number > number:
+        print('Число должно быть меньше')
+        
+    elif predict_number < number:
+        print('Число должно быть больше')
+        
+    else:
+        print(f'Вы угадали число {number} за {count} попыток!')
+        break #выход из цикла
